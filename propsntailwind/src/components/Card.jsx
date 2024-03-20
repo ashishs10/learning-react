8215d9d6a65185abf4fc4d9e566112e0322a274d
ashishs10 <ashish9six@gmail.com>
2024-03-20 18:40:03 +0530

    tailwind-props

---
+++ propsntailwind/src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div>
+      <img
+        src="https://images.pexels.com/photos/46798/the-ball-stadion-football-the-pitch-46798.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+      />
+      <h1 className="text-2xl bg-green-500 rounded">Gojo Satoru</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+  );
+}
+
+export default Card;
