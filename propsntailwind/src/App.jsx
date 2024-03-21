24f189f50d5a057aafcf65d1cd95c9e9200ac09b
ashishs10 <ashish9six@gmail.com>
2024-03-22 02:47:31 +0530

    props

--- propsntailwind/src/App.jsx
+++ propsntailwind/src/App.jsx
@@ -7,9 +7,10 @@ import Card from "./components/Card";
 function App() {
   return (
     <>
-      <h1 className="text-3xl bg-green-500 p-3 rounded-md">
+      <h1 className="text-3xl bg-green-500 p-3 rounded-t">
         React with tailwind
       </h1>
+      <Card username="Ashish Singh" post="Junior React Dev" />
       <Card />
     </>
   );
--- propsntailwind/src/components/Card.jsx
+++ propsntailwind/src/components/Card.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-function Card() {
+//
+function Card({ username = "No Name", post = "Not working" }) {
+  // console.log(props);
   return (
     <>
       {/* <img
@@ -13,14 +15,16 @@ function Card() {
         tempor incididunt ut labore et dolore magna aliqua.
       </p> */}
 
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <div className="max-w-sm overflow-hidden shadow-lg">
         <img
           className="w-full"
           src="https://images.pexels.com/photos/46798/the-ball-stadion-football-the-pitch-46798.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Sunset in the mountains"
+          alt={"Sunset in the mountains"}
         />
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+          <div className="font-bold text-xl mb-2">
+            {username} {post}
+          </div>
           <p className="text-gray-700 text-base">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Voluptatibus quia, nulla! Maiores et perferendis eaque,
